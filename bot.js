44b04ff223fce72c11372e6bdc1c66d2553ce81c
Paulo Henrique <paulohenriqe226@gmail.com>
2021-09-05 22:23:40 -0300

    update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,19 @@ const Token = 'ODgzMTE4NDI0NjEwNDM5MTg5.YTFSHw.DqR4UNGr3_trt3chvRTahznheCw';
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 const youtube = require('simple-youtube-api');
+const yt = require("discord-youtube-api");
+
+const youtube = new YouTube("google api key");
+
+async function testAll() {
+    const video1 = await youtube.getVideo("https://www.youtube.com/watch?v=5NPBIwQyPWE");
+    const video2 = await youtube.getVideoByID("5NPBIwQyPWE");
+    const video3 = await youtube.searchVideos("big poppa biggie smalls");
+    const videoArray1 = await youtube.getPlaylist("https://www.youtube.com/playlist?list=PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
+    const videoArray2 = await youtube.getPlaylistByID("PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
+
+    console.log(video1, video2, video3, videoArray1, videoArray2);
+}
 
 const filamusica = [];
 // playlist do youtube: https://www.youtube.com/playlist?list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_
@@ -29,7 +42,8 @@ bot.on('ready', () => { // when the bot is on
 /* Start of message events */
 bot.on('message', msg => {
     const m = msg.content.toLowerCase(); // m = message content in lower case
-    var idvoice = msg.member.voiceChannel; // idvoice = the info about channel that the msg sender is
+    let idvoice = msg.member.voiceChannel; // idvoice = the info about channel that the msg sender is
+
     if (msg.author.bot) return; // if the message is from another bot
     if (msg.channel.type === 'dm') return; // if is a DirectMessage
     if (m === '!avril')
@@ -46,7 +60,6 @@ bot.on('message', msg => {
             }); // bot join
             console.log("Bot joined at: "+idvoice.name+" in: "+idvoice.guild.name);
             msg.channel.send("Let's do it!");
-
         }
         else {
             return msg.channel.send("Não ta no canal de voz né pô, faz certo, faz direito!");
@@ -60,6 +73,20 @@ bot.on('message', msg => {
                 connection.playArbitraryInput(stream);
             })
         }
+    }
+    if (m === 't'){
+        if (idvoice != null){
+            idvoice.join().then(connection =>{
+                const stream = ytdl('https://www.youtube.com/watch?v=5NPBIwQyPWE&list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_&index=1&ab_channel=AvrilLavigneVEVO');
+                let x = ytpl.getPlaylistID('https://www.youtube.com/watch?v=5NPBIwQyPWE&list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_&index=1&ab_channel=AvrilLavigneVEVO');
+
+                console.log(x);
+
+                idvoice.join();
+                connection.playArbitraryInput(stream);
+            })
+        }
+
     }
     if (m === '!comandos')
         return msg.channel.send('!avril\n!botfdp\n!gata\n!justdoit or !jdi\n!justvitao\n😘😘😜\n');
