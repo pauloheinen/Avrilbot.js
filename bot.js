06a6b1a052bd47bf06949172a5f75f112c2aaeaa
Paulo Henrique <paulohenriqe226@gmail.com>
2021-09-10 00:04:27 -0300

    bot V0.2 voice state bugged

--- bot.js
+++ bot.js
@@ -12,6 +12,10 @@ const downloader = require("@discord-player/downloader").Downloader;
 
 /* BOT CONSTANTS */
 const LOCAL = './Music/';
+const playlists = ['https://www.youtube.com/playlist?list=PLED03B2E1FC47994B',
+    'https://www.youtube.com/playlist?list=PLOfcxmVI3iUASS5tHevLxLfGq3lZJYrNh',
+    'https://www.youtube.com/playlist?list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_']
+const random = Math.floor(Math.random() * playlists.length);
 let radioON = false;
 
 /* Connection */
@@ -24,36 +28,36 @@ http.createServer(function (req, res) {
 bot.on('ready', () => { // when the bot is on
     console.log('Bot Alive!')
     setInterval(() => bot.user.setActivity(`Avril Lavigne`, {
-        type: "LISTENING",
-        url: "https://www.youtube.com/playlist?list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_"
+        type: "LISTENING"
     }), 5000)
 })
 
 /* Message event */
 bot.on('message', msg => {
-    const m = msg.content.toLowerCase(); // m = message content in lower case
+    let m = msg.content.toLowerCase(); // m = message content in lower case
     let idvoice = msg.member.voiceChannel; // idvoice = the info about channel that the msg sender is
 
     if (msg.author.bot) return 0;  // if the message is from another bot
     else if (msg.channel.type !== 'text') return 0; // if isn a text message
-    else if (m === '!avril')
+    if (m === '!Avril')
         return msg.channel.send('Hey Hey You You');
-    else if (m === '!botfdp')
+    if (m === '!botfdp')
         return msg.reply('fdp é teu pai, aquele corno');
     else if (m === '!gata')
         return msg.channel.send('gata gorda 🙀');
     else if (m === '!justdoit' || m === '!jdi' || m === '!justvitao' || m === 't'){
         if (idvoice != null) { // if is in a voice channel
             if (radioON === false){
-                radioON = true;
                 return msg.channel.send("Let's do it!").then(playmusic(idvoice));
             }
+            else
+                return msg.channel.send("I'm already playing something!");
         }
         else
             return msg.channel.send("Não ta no canal de voz né pô, faz certo, faz direito!");
     }
     else if (m === '!comandos')
-        return msg.channel.send('!avril\n!botfdp\n!gata\n!justdoit or !jdi\n!justvitao\n😘😘😜\n');
+        return msg.channel.send('!Avril\n!botfdp\n!gata\n!justvitao\n!jdi or !justdoit\n!comandos\n😘😘😜😎😎\n');
 
 });
 
@@ -62,11 +66,9 @@ bot.on('message', msg => {
 /* Voice State event */
 bot.on("voiceStateUpdate", (oldMember, newMember) => {
     if (newMember.voiceChannelID === '883096070857556009') { // if some member enters in voice channel with that id
-        newMember.voiceChannel.join().then(connection => {
-            const stream = ytdl('https://www.youtube.com/watch?v=LOcKckBLouM&list=PLOfcxmVI3iUASS5tHevLxLfGq3lZJYrNh&ab_channel=AvrilLavigneVEVO');
-            connection.playArbitraryInput(stream);
-        });
-        console.log(newMember.user.username+" Entrou no canal Avril 24/7");
+        playmusic(newMember.voiceChannel);
+        return console.log(newMember.user.username+" Entrou no canal Avril 24/7")
+
     }
     if (oldMember.voiceChannelID === '883096070857556009') // if some member leave that same voice channel
         console.log(oldMember.user.username+" Saiu do canal Avril 24/7");
@@ -84,15 +86,40 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
 function playmusic(idvoice){
     idvoice.join()
         .then(connection =>{
-            youtube.getPlaylist('https://www.youtube.com/playlist?list=PLH69otCpA08EF1LACrijzjkEu9NzCvEr_')
+            youtube.getPlaylist(playlists[random])
                 .then(playlist =>{
                     playlist.getVideos()
                         .then(videos => {
-                            verifystorage(videos);
-                            stream(connection, videos, videos.length - videos.length);
+                            beta(videos, connection, videos.length-videos.length);
+                            //verifystorage(videos);
+                            //stream(connection, videos, videos.length - videos.length);
                         })
                 })
         })
+}
+
+
+function beta(videos, connection, index){
+    fs.readdir(LOCAL, (async (err, files) => { // where to search for
+        if (files.includes(videos[index].title + ".mp3") === true){
+            console.log("Already has: " + videos[index].title + ".mp3");
+            console.log('Now playing: ' + videos[index].title);
+            radioON = true;
+            let dispatcher = await connection.playFile(LOCAL+videos[index].title+".mp3");
+            await dispatcher.on('end', () => {
+                radioON = false;
+                dispatcher = undefined;
+                beta(videos, connection, index += 1);
+            })
+        }
+        else{
+            console.log("Downloading: " + videos[index].title + ".mp3");
+            const stream = downloader.download(videos[index].url);
+            await new Promise(r => setTimeout(r, 5000));
+            stream.pipe(fs.createWriteStream(LOCAL + videos[index].title + ".mp3"));
+            beta(videos, connection, index);
+        }
+    }));
 }
 
 function verifystorage(videos){
